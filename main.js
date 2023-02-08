c76238279c9276891260e4f41481ba06a0fd7d81
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-02-07 22:35:48 -0500

    adds background colour

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
+function setTitle(){
 document.getElementById("main-h1").innerHTML = "New Title";
+}
+setTitle()
 
+function setDescription(){
+    document.querySelector("p").innerHTML = "New Description of Consolle Constroller";
+}
+setDescription()
+
+function setBackgroundColor(){
+    document.body.style.backgroundColor = "lightpink";
+}
+setBackgroundColor()
