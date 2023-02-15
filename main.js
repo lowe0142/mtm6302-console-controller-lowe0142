5a272b0ff09ebacc105814c2f3c074d331cca612
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-02-15 16:40:53 -0500

    sets dark and light mode

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 function setTitle(){
     const title = document.getElementById("main-h1").innerHTML = "New Title";
-   }
-   setTitle()
+}
+setTitle()
    
-   function setDescription(){
-      const description =document.querySelector("p").innerHTML = "New Description of Console Constroller";
-   }
-   setDescription()
-   
-   function setBackgroundColor(){
-       const bckg =  document.body.style.backgroundColor = "lightpink";
-    }
-    setBackgroundColor()
+ function setDescription(){
+    const description = document.querySelector("p").innerHTML = "New Description of Console Constroller";
+}
+setDescription()
+
+function setBackgroundColor(){
+    const bckg =  document.body.style.backgroundColor = "lightpink";
+}
+setBackgroundColor()
     
-   function setFontColour(){
-       const font = document.getElementById("main").style.color = "green";
-   }
-   setFontColour()
+function setFontColour(){
+    const font = document.getElementById("main").style.color = "green";
+}
+setFontColour()
+
+function setTheme(){
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+}
 
