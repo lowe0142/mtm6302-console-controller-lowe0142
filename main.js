ca20566a03539a3384e0dc21556410ab3992e202
Gabrielle Lowe <97476695+lowe0142@users.noreply.github.com>
2023-02-16 03:43:31 -0500

    completes assignment

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 function setTitle(){
     const title = document.getElementById("main-h1").innerHTML = "New Title";
 }
-setTitle()
+
    
  function setDescription(){
-    const description = document.querySelector("p").innerHTML = "New Description of Console Constroller";
+    const description = document.querySelector("p").innerHTML = "This allows for user interaction without the need for an interface.";
 }
-setDescription()
+
 
 function setBackgroundColor(){
     const bckg =  document.body.style.backgroundColor = "lightpink";
 }
-setBackgroundColor()
+
+
     
 function setFontColour(){
     const font = document.getElementById("main").style.color = "green";
 }
-setFontColour()
+
 
 function setTheme(){
     let element = document.body;
     element.classList.toggle("dark-mode");
 }
-setTheme()
+
 
